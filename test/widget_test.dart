0c9676f2744c97fe00c6845e61cced7a0d10f23e
ohyeah2021 <1715071962@qq.com>
2026-03-18 19:07:01 +0800

    删除无关代码

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:lume_lucy_paperwall/main.dart';
-
-void main() {
-  testWidgets('App starts correctly', (WidgetTester tester) async {
-    await tester.pumpWidget(const NivoluneApp());
-    expect(find.text('开始探索'), findsOneWidget);
-  });
-}
